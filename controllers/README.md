44a2e8f97eceb0eddcba1997ea025920a4f8c026
EzequielJavaS <68125532+EzequielJavaS@users.noreply.github.com>
2020-12-26 18:12:39 +0100

    Backed 1,0  Calendar-app terminado

---
+++ controllers/README.md
@@ -0,0 +1,5 @@
+# Backend MERN - Calendar
+
+Backend para Calendar-app
+
+Ezequiel del Río
--- controllers/events.js
+++ controllers/events.js
@@ -39,19 +39,101 @@ const crearEvento =async (req, res = response) => {
     }
 }
 
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg:'actualizarEvento'
-    })
+const actualizarEvento = async (req, res = response) => {
+
+    //Accedo al id del evento Este valor viene del valor que mando en la url
+    const eventoId = req.params.id;
+    const uid = req.uid;
+  
+    //Verificar si la id existe en la base de datos
+    
+    try {
+        //Utilizamos el modelo de mongoose
+        const evento = await Evento.findById( eventoId );
+        console.log(evento.user.toString());
+        console.log(uid);
+
+        if ( !evento ){ //Si no existe el evento
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if (evento.user.toString() !== uid ){ //Si el usuario identificado no es el mismo que el que creó enevento
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios de editar este evento'
+            })
+        }
+
+        //El evento existe y el usuario es el correcto:
+        const nuevoEvento = { //desestructuro lo que hay en la request.body, que es el evento modificado
+            ...req.body,
+            user: uid //Le añado la propiedad user dado que la request no la tiene y el modelo Evento lo requiere
+        }
+
+        //Si  no ponemos { new: true } devolverá e elemeto original. Esto nos devolverá el nuevo elemento
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true });
+
+        res.json({
+            ok:true,
+            evento: eventoActualizado
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg: "hable con el administrador"
+        });
+    }
 }
 
- const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg:'eliminarEvento'
-    })
+const eliminarEvento = async (req, res = response) => {
+
+    //Accedo al id del evento Este valor viene del valor que mando en la url
+    const eventoId = req.params.id;
+    const uid = req.uid;
+  
+    //Verificar si la id existe en la base de datos
+    
+    try {
+        //Utilizamos el modelo de mongoose
+        const evento = await Evento.findById( eventoId );
+
+        if ( !evento ){ //Si no existe el evento
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        if (evento.user.toString() !== uid ){ //Si el usuario identificado no es el mismo que el que creó enevento
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios de eliminar este evento'
+            })
+        }
+        
+        const eventoEliminado = await Evento.findByIdAndRemove ( eventoId );
+
+        res.json({
+            ok:true,
+            evento: "eliminado"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg: "hable con el administrador"
+        });
+    }
 }
+
+    
+
 
 
 module.exports = {
